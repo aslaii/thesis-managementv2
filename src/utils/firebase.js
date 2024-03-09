f5ea2e9ad0016a5ad67c03786bf705227e6b45b2
Jericho Bermas <jecho@aslaii.com>
2024-03-09 15:29:31 +0800

    Cleaned used Javascript Version, Initial Commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyATx_pfWZpe2-L3QNi3UKLgEZ6h8AlTkCI',
+  authDomain: 'thesis-management-5e662.firebaseapp.com',
+  projectId: 'thesis-management-5e662',
+  storageBucket: 'thesis-management-5e662.appspot.com',
+  messagingSenderId: '5901023680',
+  appId: '1:5901023680:web:0309b90811ff28313a7025'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
